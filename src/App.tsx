99c6d96a2563317b6854f44ab891282f2dd715d7
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-04-16 18:27:50 +0100

    add movies list ui

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
             on your previous watch history.
           </h1>
         </div>
-        <div className="h-20"></div>
+        <div className="h-10"></div>
         <div className="flex justify-center  w-full gap-4">
           <ComboBox />
 
@@ -24,6 +24,27 @@ function App() {
             Add Movie
           </button>
         </div>
+        <div className="h-10"></div>
+
+        <div className="bg-gray-800 text-white text-center p-4 w-full">
+          <h1 className="text-2xl">Selected Movies</h1>
+        </div>
+        <div className="flex justify-center flex-wrap">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div
+              key={index}
+              className="flex flex-col w-1/6 bg-gray-200 p-4 m-4 rounded-lg"
+            >
+              <img src="https://via.placeholder.com/150" alt="movie" />
+              <h1 className="text-center text-xl">Movie Name</h1>
+            </div>
+          ))}
+        </div>
+        {/* add button  */}
+        <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded self-center text-2xl">
+          Generate Recommendations
+        </button>
+        <div className="h-4"></div>
       </div>
     </>
   );

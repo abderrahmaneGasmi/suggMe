4f254f282d519d51eca26b26064ad1b300e6984d
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-04-18 13:39:00 +0100

    add recommendation based on score

--- backend/recomandation.py
+++ backend/recomandation.py
@@ -19,14 +19,13 @@ def removewhitesspacesinlist(listsw):
 def getrecomandadbasedonfiltering(selected_movies):
 
 
-
+    
 
     # Aggregate selected movie attributes
     # 'original language'  'genres' ' production company ' 'keywords'  'vote_average' 'vote_cont' 
     selected_original_languages = removeduplicateitemsinlist([movie['original_language'] for movie in selected_movies])
-    selected_genres = removewhitesspacesinlist([genre for movie in selected_movies for genre in movie['genres']])
+    selected_genres = removeduplicateitemsinlist(removewhitesspacesinlist([genre for movie in selected_movies for genre in movie['genres']]))
     selected_production_companies = removewhitesspacesinlist([movie['production_companies'] for movie in selected_movies])
-    selected_keywords = [keyword for movie in selected_movies for keyword in movie['keywords']]
     # Convert genres to binary representation using one-hot encoding
     mlb = MultiLabelBinarizer()
     genres_encoded = mlb.fit_transform(movies_df['genres'])
@@ -53,44 +52,98 @@ def getrecomandadbasedonfiltering(selected_movies):
 
         
         ]
-    selected_features = ['vote_average', 'vote_count']  # Example features used for similarity calculation
-    selected_movies_features = filtered_movies_df[selected_features]
-    all_movies_features = movies_df_encoded[selected_features]
 
-    similarity_scores = cosine_similarity(selected_movies_features, all_movies_features)
-
-    top_recommendations_indices = similarity_scores.argsort()[0][::-1]  # Indices of most similar movies
-    top_recommendations_titles = movies_df.iloc[top_recommendations_indices]
-    # remove selected movies from the top recommendations
-    top_recommendations_titles = top_recommendations_titles[~top_recommendations_titles['title'].isin([movie['title'] for movie in selected_movies])]
-    
     filtered_movies_df = filtered_movies_df[~filtered_movies_df['title'].isin([movie['title'] for movie in selected_movies])]
-    return filtered_movies_df
+    filtered_movies_df = filtered_movies_df.sort_values(by='vote_average', ascending=False)
+    
+
+    return filtered_movies_df[['title' , 'genres','vote_average','poster_path']]
 
 def getmoviesfromtitles(titles):
     return movies_df[movies_df['title'].isin(titles)].to_dict('records')
+def similairitemscountintwolist(list1,list2):
+    return len(set(list1) & set(list2)) 
 
-app = Flask(__name__)
-
-CORS(app)
-# Define routes and functions
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-@app.route('/movies', methods=[ 'POST'])
-def get_movies():
-    movies = request.json
-    # convert movies to dictionary
-    # movies = [movie.to_dict() for movie in movies]
-    moviesdemanded = getmoviesfromtitles(
-        [movie['title'] for movie in movies]
-    )
-    predictions = getrecomandadbasedonfiltering(moviesdemanded)
-    # return 10 items from the predictions
-    return jsonify(predictions[:10].to_dict('records'))
+def gestrecomandadbasedonscore(selected_movies):
+    # 'original language'  'genres' ' production company ' 'keywords'  'vote_average' 'vote_cont' 
+    selected_original_languages = removeduplicateitemsinlist([movie['original_language'] for movie in selected_movies])
+    selected_genres =removeduplicateitemsinlist( removewhitesspacesinlist([genre for movie in selected_movies for genre in movie['genres']]))
+    selected_production_companies =  removewhitesspacesinlist([movie['production_companies'] for movie in selected_movies])
+    selected_keywords = [movie['keywords'].split(',') for movie in selected_movies ]
+    flattened_keywords = [keyword for sublist in selected_keywords for keyword in sublist]
+    unique_keywords = removeduplicateitemsinlist(flattened_keywords)
+    trimmed_keywords = removewhitesspacesinlist(unique_keywords)
+    selected_production_countries = [movie['production_countries'].split(',') for movie in selected_movies]
+    flattened_production_countries = [country for sublist in selected_production_countries for country in sublist]
+    unique_production_countries = removeduplicateitemsinlist(flattened_production_countries)
+    trimmed_production_countries = removewhitesspacesinlist(unique_production_countries)
+    spoken_languages = [movie['spoken_languages'].split(',') for movie in selected_movies ]
+    flattened_spoken_languages = [keyword for sublist in spoken_languages for keyword in sublist]
+    unique_spoken_languages = removeduplicateitemsinlist(flattened_spoken_languages)
+    trimmed_spoken_languages = removewhitesspacesinlist(unique_spoken_languages)
 
 
+    keyword_weight = 3
+    genre_weight = 5
+    spoken_languages_weight = 4
+    moviesmin = movies_df[['title' , 'genres','vote_average','poster_path']]
+    for index, row in movies_df.iterrows():
+        score = similairitemscountintwolist(row['genres'],selected_genres) * genre_weight
+        score += similairitemscountintwolist(
+            removeduplicateitemsinlist(
+                removewhitesspacesinlist(
+                    row['keywords'].split(',')
+                )
+            ),trimmed_keywords
+        )* keyword_weight
+        score += similairitemscountintwolist(
+            removeduplicateitemsinlist(
+                removewhitesspacesinlist(
+                    row['spoken_languages'].split(',')
+                )
+            ),trimmed_spoken_languages
+        )* spoken_languages_weight
+        if(row['original_language'] in selected_original_languages):
+            score+=1
+        if(row['production_companies'] in selected_production_companies):
+            score +=4
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+    
+    # Add to the score based on the number of selected genres
+    # score += len(set(selected_genres).intersection(row['genres'])) * genre_weight
+    
+    # Assign the calculated score to the 'score' column
+        moviesmin.loc[index, 'score'] = score
+
+        
+    return moviesmin.sort_values(by=['score', 'vote_average'], ascending=False).drop(columns=['score'])
+
+recomandmovies = gestrecomandadbasedonscore(selected_moviesws)
+recomandmovies2 = getrecomandadbasedonfiltering(selected_moviesws)
+print(recomandmovies.head())
+print(recomandmovies2.head())
+# app = Flask(__name__)
+
+# CORS(app)
+# # Define routes and functions
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
+
+# @app.route('/movies', methods=[ 'POST'])
+# def get_movies():
+#     movies = request.json
+#     # convert movies to dictionary
+#     # movies = [movie.to_dict() for movie in movies]
+#     moviesdemanded = getmoviesfromtitles(
+#         [movie['title'] for movie in movies]
+#     )
+#     predictions = getrecomandadbasedonfiltering(moviesdemanded)
+#     # return 10 items from the predictions
+#     return jsonify(predictions[:10].to_dict('records'))
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
